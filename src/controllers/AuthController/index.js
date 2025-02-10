e43d4f7c98832e73187c0bcb5083c7231326a64a
yamaankhan20 <khanyamaan1@gmail.com>
2025-02-10 07:31:47 +0500

    register api developed

--- src/controllers/AuthController/index.js
+++ src/controllers/AuthController/index.js
@@ -4,12 +4,31 @@ const User = require('../../models/users');
 
 
 Register = async ( req, res )=>{
-    const { name, email, password} = req.body;
+    const { name, email, password, role, PhoneNumber, IndustryType, JobTitle, Description} = req.body;
     try{
-        if(!name){
-            res.status(401).json({error_message:"not found"})
+        if (!name || !email || !password) {
+            return res.status(400).json({ error_message: "Name, email, and password are required." });
         }
-        res.status(200).json({ message: `${email}` });
+        const existingOne = await User.findOne({ where: { email } } );
+        if(existingOne){
+            return res.status(400).json({ error_message: "User Already Exist!" });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            role,
+            PhoneNumber,
+            IndustryType,
+            JobTitle,
+            Description
+        });
+
+        res.status(201).json({ message: "User registered successfully!", user: newUser,});
+
+
     }catch (e){
         res.status(500).json({error_message: e.message});
     }

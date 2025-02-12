e3d10d4b5ebfa0e5a7eaa59d52e007f972897821
Sohaib-px <sohaibahmed182002@gmail.com>
2025-02-12 19:54:24 +0500

    user api changes

--- src/controllers/UserController/index.js
+++ src/controllers/UserController/index.js
@@ -5,9 +5,10 @@ const User = require('../../models/users');
 
 get_all_users = async ( req, res )=>{
     const { industry_type } = req.params;
+    const user_id = req.user?.userId;
     try{
-
-        res.status(200).json({ message: `sa` });
+        const users = await User.findOne({ where: { id: user_id } });
+        res.status(200).json({ message: `${users.role}` });
     }catch (e){
         res.status(500).json({error_message: e.message});
     }

9b0acb5a4f9980ee974c0ce7ba07a96cb4817d33
yamaankhan20 <khanyamaan1@gmail.com>
2025-02-26 16:45:52 +0500

    yml changed

--- src/models/virtualclinic.js
+++ src/models/virtualclinic.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       {
         sequelize,
-        modelName: 'VirtualClinic',
+        modelName: 'VirtualClinic' ,
         tableName: 'VirtualClinics', // **Table Name Set Karo**
         timestamps: true,
       }

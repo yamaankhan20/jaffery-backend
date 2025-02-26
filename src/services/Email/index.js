ca919a7ac8f36373d2f1a1647819d099a956c335
yamaankhan20 <khanyamaan1@gmail.com>
2025-02-26 18:36:17 +0500

    yml changed

--- src/services/Email/index.js
+++ src/services/Email/index.js
@@ -9,9 +9,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    tls: {
-        rejectUnauthorized: false, // Prevents SSL issues
-    }
+    // tls: {
+    //     rejectUnauthorized: false, // Prevents SSL issues
+    // }
 });
 
 module.exports = transporter;

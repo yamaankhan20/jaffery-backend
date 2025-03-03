0bca7b0bb864b53951b4ee52daae9a54715c1b5d
yamaankhan20 <khanyamaan1@gmail.com>
2025-03-03 18:06:11 +0500

    business network added

--- src/routes/issue-forms/index.js
+++ src/routes/issue-forms/index.js
@@ -6,5 +6,6 @@ router.post('/legal-assistance', UserController.legal_issues);
 router.post('/virtual-clinic', UserController.virtual_clinic);
 router.post('/professional-network', UserController.professional_network);
 router.post('/business-network', UserController.business_network);
+router.post('/get-business-network', UserController.get_business_network);
 
 module.exports = router;

ff9e9aa46bcce2bd1b7a53874f451fdc2445f96b
yamaankhan20 <khanyamaan1@gmail.com>
2025-03-10 20:12:40 +0500

    image Api developed

--- src/app.js
+++ src/app.js
@@ -10,11 +10,12 @@ const path = require("path");
 app.use(express.json());
 app.use(cors());
 
+app.use("/public/uploads", express.static(path.join(__dirname, "src/public/uploads")));
+
 app.use(AuthMiddleware);
 app.use('/api', routes);
 app.use(MethodNotAllowed);
 
-app.use("/public/uploads", express.static(path.join(__dirname, "src/public/uploads")));
 
 
 module.exports = app;

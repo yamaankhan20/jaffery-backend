799c8ff87b83ac3ec9813f3ea0b5f46a1707d396
yamaankhan20 <khanyamaan1@gmail.com>
2025-03-10 21:27:12 +0500

    image Api developed

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
-console.log("Serving static files from:", path.join(__dirname, "public/uploads"));
+console.log("Serving static files from:", express.static(path.join(__dirname, "src/public/uploads")));
 
 app.use(AuthMiddleware);
 app.use('/api', routes);

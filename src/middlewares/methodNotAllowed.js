a626b1b8be03e5bb322fcada6c611cbebd04d358
yamaankhan20 <khanyamaan1@gmail.com>
2025-03-10 20:09:47 +0500

    image Api developed

--- src/middlewares/methodNotAllowed.js
+++ src/middlewares/methodNotAllowed.js
@@ -1,4 +1,8 @@
 module.exports = (req, res, next) => {
+    if (req.path.startsWith('/public/uploads')) {
+        return next();
+    }
+
     if (req.method !== "POST") {
         return res.status(405).json({
             message: "This Method Is Not Allowed!"
